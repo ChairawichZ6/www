d9757bedc71905f43f233c651a28a2c9af4de992
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-10-30 12:57:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   
      <p>Yesterday was my 18th birthday!” a customer said after walking into our convenience store. He then asked for some e-cigarette products and handed me his ID to prove he was indeed of age.</p>
      <p>I scanned the ID, but it came back expired. Now thoroughly deflated, he asked, “Does that mean I’m not 18?” —David Hansen</p>
-     <img src=\"cat.jpg\">
+     <img src="cat.jpg">
      <img src="cat1.jpg">
 
   <br><a href="http://www.it.kmitl.ac.th">This is My favorite Link</a>
