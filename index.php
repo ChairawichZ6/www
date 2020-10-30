db5e15204dd67a95433a34470191e78b622cafdf
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-10-30 12:52:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <body>
   <?php
     echo "<h2>ชัยรวิชญ์ เอี่ยมขจรชัย ฟลุ๊ค</h2>";
-    echo "“Yesterday was my 18th birthday!” a customer said after walking into our convenience store. He then asked for some e-cigarette products and handed me his ID to prove he was indeed of age."<br>
-    echo " I scanned the ID, but it came back expired. Now thoroughly deflated, he asked, “Does that mean I’m not 18?” —David Hansen"<br>
+    echo '“Yesterday was my 18th birthday!” a customer said after walking into our convenience store. He then asked for some e-cigarette products and handed me his ID to prove he was indeed of age.'<br>;
+    echo " I scanned the ID, but it came back expired. Now thoroughly deflated, he asked, “Does that mean I’m not 18?” —David Hansen"<br>;
     echo "<img src=\"cat.jpg\">";
     echo '<img src="cat1.jpg">';
   ?>
