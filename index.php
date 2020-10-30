b754995de7720a9150380c38a3a12a020946bce0
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-10-30 12:34:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-echo"<h2>IT@KMITL</h2>";
-echo"Hello IT<br>";
-?>
+<html>
+<head>
+  <titile>Test</title>
+</head>
+<body>
+  <?php
+    echo "<h2>ชัยรวิชญ์ เอี่ยมขจรชัย ฟลุ๊ค</h2>";
+    echo "<img src=\"cat.jpg\">";
+    echo '<img src="cat1.jpg">';
+  ?>
+</body>
+</html>
