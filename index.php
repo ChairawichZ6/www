09a980eb135b409fc0596992c4af366d531dfecd
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-10-30 12:56:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
   <title>Fook_63070034</title>
 </head>
 <body>
-  <?php
-    echo "<h2>ชัยรวิชญ์ เอี่ยมขจรชัย ฟลุ๊ค</h2>";
-    echo '“Yesterday was my 18th birthday!” a customer said after walking into our convenience store. He then asked for some e-cigarette products and handed me his ID to prove he was indeed of age.'<br>;
-    echo '" I scanned the ID, but it came back expired. Now thoroughly deflated, he asked, “Does that mean I’m not 18?” —David Hansen"<br>';
-    echo "<img src=\"cat.jpg\">";
-    echo '<img src="cat1.jpg">';
-  ?>
+
+    <h2>ชัยรวิชญ์ เอี่ยมขจรชัย ฟลุ๊ค</h2>
+  
+     <p>Yesterday was my 18th birthday!” a customer said after walking into our convenience store. He then asked for some e-cigarette products and handed me his ID to prove he was indeed of age.</p>
+     <p>I scanned the ID, but it came back expired. Now thoroughly deflated, he asked, “Does that mean I’m not 18?” —David Hansen</p>
+     <img src=\"cat.jpg\">
+     <img src="cat1.jpg">
+
   <br><a href="http://www.it.kmitl.ac.th">This is My favorite Link</a>
   
   
