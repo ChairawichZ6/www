1e836310867ad3b686a984c736b0dfc7dd54357e
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-10-30 12:43:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 </head>
 <body>
   <?php
-    echo "<h2>ชัยรวิชญ์ เอี่ยมขจรชัย ฟลุ๊ค</h2>";
+    echo "<h2>ชัยรวิชญ์ เอี่ยมขจรชัย fook</h2>";
     echo "<img src=\"cat.jpg\">";
     echo '<img src="cat1.jpg">';
   ?>
   <a href="http://www.it.kmitl.ac.th">This is a link</a>
+  
 </body>
 </html>
