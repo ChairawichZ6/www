e10172e601e8e3b60cf20de0a5e8f447f3e1174e
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-11-18 20:33:46 +0700

    Update show.php

--- show.php
+++ show.php
@@ -16,6 +16,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
+    <th scope="col" width="350"> <div align="center">Link </div></th>
     <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
@@ -25,6 +26,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
     <td>
         <input type="button" value="แก้ไข" onclick="window.location.href='https://webfook.azurewebsites.net/edit.php?name=<?php echo $Result['Name'];?>&comment=<?php echo $Result['Comment'];?>&link=<?php echo $Result['Link'];?>'" /> 
         <input type="button" value="ลบ" onclick="window.location.href='https://webfook.azurewebsites.net/delete.php?rn=<?php echo $Result['Name'];?>'" />
@@ -39,8 +41,11 @@ mysqli_close($conn);
 ?>
 </body>
 <form>
-            <a href="https://webfook.azurewebsites.net/Form.html"><button type="button">เพิ่ม</button></a>
+            <input class="btn btn-success" type="button" value="เพิ่ม" onclick="window.location.href="https://webfook.azurewebsites.net/Form.html" />
 </form>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 <style>
 body {
   background-image: url('it kmitl.jpg');
