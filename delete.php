c91e0412940de72fd96f83c835959f2229fd5c42
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-11-19 12:32:28 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body class="container text-center" style="height: 100vh; background:url("https://mdbootstrap.com/img/Photos/Horizontal/Nature/full page/img(11).jpg");background-repeat: no-repeat;background-size: cover;">
+<body class="container text-center" style="height: 100vh; background:url(https://images.unsplash.com/photo-1502614106407-f0b9eca73d6b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80");background-repeat: no-repeat;background-size: cover;">
   <div class="h-100" style="display:grid; align-items: center;">
     <div class="roundred p-3 shadow">
 <?php
