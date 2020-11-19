de82919f191e584efe1378a955299ffee7615394
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-11-19 12:20:12 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="container text-center" style="height: 100vh; background: url("https://mdbootstrap.com/img/Photos/Horizontal/Nature/full page/img(11).jpg");background-repeat: no-repeat;background-size: cover;">
+  <div class="h-100" style="display:grid; align-items: center;">
+    <div class="roundred p-3 shadow">
 <?php
 
 $conn = mysqli_init();
@@ -10,17 +21,10 @@ if (mysqli_connect_errno($conn))
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
-$id = '';
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$id = $_POST['stid'];
  
-while($Result = mysqli_fetch_array($res))
-{
-    if ($Result['Name'] == $name){
-        $id = $Result['ID'];
-    }
-}
 $sql = "UPDATE guestbook 
-        SET Comment='$comment', Link='$link'
+        SET Name='$name', Comment='$comment', Link='$link'
         WHERE ID='$id'";
 
 if (mysqli_query($conn, $sql)) {
@@ -31,3 +35,13 @@ if (mysqli_query($conn, $sql)) {
   
 mysqli_close($conn);
 ?>
+<div class="my-5">
+<input class="btn btn-success" type="button" value="กลับหน้าหลัก" onclick="window.location.href='https://webfook.azurewebsites.net/show.php'" />
+</div>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>    
+</body>
+</html>
